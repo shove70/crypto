0a27fde14bf99eec460593427c4e2dc02971a723
shove70 <shove@163.com>
2018-06-02 16:30:48 +0800

    Add a example.

---
+++ examples/rsakey/src/app.d
@@ -0,0 +1,34 @@
+import std.conv: to;
+import std.stdio;
+
+import crypto.rsa;
+
+void main(string[] argv)
+{
+    int bits = 256;
+    
+    if (argv.length > 1)
+    {
+        try
+        {
+            bits = argv[1].to!int;
+        }
+        catch(Throwable)
+        {
+            writeln("Parameter " ~ argv[1] ~ " error. Usage: 128 | 256 | 512 | 1024 ...");
+            return;
+        }
+        
+        if (bits < 128)
+        {
+            writeln("Parameter " ~ argv[1] ~ " too small and needs 128 minimum.");
+            return;
+        }
+    }
+    
+    RSAKeyPair keyPair = RSA.generateKeyPair(bits);
+    writeln("PrivateKey:");
+    writeln(keyPair.privateKey);
+    writeln("\r\nPublicKey:");
+    writeln(keyPair.publicKey);
+}
